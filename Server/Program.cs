3e432c849caf25f409bc8bc628e8b46fefe2f6e4
Driadix <DriadixA@yandex.ru>
2025-06-16 02:08:18 +0300

    feat(api): Setup ASP.NET Core Web API for client-server communication

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Veterinary Clinic Server");
+﻿using BusinessLogic.Services;
+using BusinessLogic.Strategies;
+using Core.Interfaces;
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IDataAccessStrategy, EfStrategy>();
+
+builder.Services.AddScoped<ClinicService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/api/clients", async (ClinicService clinicService) =>
+{
+    try
+    {
+        var clients = await clinicService.GetAllClientsAsync();
+        return Results.Ok(clients);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Произошла ошибка на сервере: " + ex.Message);
+    }
+});
+
+app.Run();
